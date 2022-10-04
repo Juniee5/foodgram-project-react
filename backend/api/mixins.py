98f090c499433b847fb766f674a20d78a73172d3
Juniee5 <96186923+Juniee5@users.noreply.github.com>
2022-10-04 13:25:28 +0700

    Update mixins.py

--- backend/api/mixins.py
+++ backend/api/mixins.py
@@ -3,7 +3,6 @@ from rest_framework.permissions import AllowAny
 
 from api.permissions import IsAdminOrReadOnly
 from recipes.models import Recipe
-
 from .serializers import SubscribeRecipeSerializer
 
 
