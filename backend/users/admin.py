a8057b6e9a794c44a62d52e9140dabb823668024
Juniee5 <96186923+Juniee5@users.noreply.github.com>
2022-10-08 19:04:10 +0700

    Update admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+from .models import Subscribe
+
+
+EMPTY_MSG = '-пусто-'
 User = get_user_model()
 
 
@@ -12,3 +16,12 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ('email', 'username', 'first_name', 'last_name')
     list_filter = ('date_joined', 'email', 'first_name')
     empty_value_display = '-пусто-'
+
+    
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user', 'author', 'created',)
+    search_fields = (
+        'user__email', 'author__email',)
+    empty_value_display = EMPTY_MSG
